7d5a6c39bd1b830c82f839382e39d7ca990ee75a
nandkishorhiwale321 <nandkishoehiwale321@gmail.com>
2025-09-12 13:34:24 +0530

    leap year program

---
+++ leapYear.java
@@ -0,0 +1,16 @@
+package Java_Pack;
+
+public class leapYear {
+
+	public static void main(String[] args) {
+		int year = 2024;
+		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+			System.out.println(year + " is a leap year.");
+		}
+		else {
+			System.out.println(year  +  "is not a leap year.");
+		}
+
+	}
+
+}
