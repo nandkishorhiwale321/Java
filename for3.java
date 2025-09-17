636c16ca2509ab368c6e68bd48e3353a0e73b5bb
nandkishorhiwale321 <nandkishoehiwale321@gmail.com>
2025-09-17 12:20:48 +0530

    Table of 1 to 10

---
+++ for3.java
@@ -0,0 +1,17 @@
+package Java_Pack;
+
+public class for3 {
+
+	public static void main(String[] args) {
+		
+for (int num = 1; num <= 10; num++) {
+	System.out.println("Table of"  +  num  +  ":" );
+
+for (int k = 1; k <= 10; k++) {
+	System.out.println(num + "X" + k + "=" + num * k);  
+	}
+System.out.println("***********");
+}
+
+}
+}
