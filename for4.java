5a816c0a98aba7ed06affa84f9b2970c6f1ac153
nandkishorhiwale321 <nandkishoehiwale321@gmail.com>
2025-09-17 12:16:46 +0530

    summtion of  number using for

---
+++ for4.java
@@ -0,0 +1,16 @@
+package Java_Pack;
+
+public class for4 {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		int sum = 0 ;
+		for(int num = 1; num <= 5; num++) {
+			 sum = sum + num;
+			// System.out.println(sum);
+			
+		}
+		System.out.println(sum);
+	}
+
+}
